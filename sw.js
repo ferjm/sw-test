71e7e150d4cc9d521844119826f9e7ffc047eb5f
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2015-01-09 12:05:41 +0100

    Do not use cache

--- sw.js
+++ sw.js
@@ -4,22 +4,6 @@ function debug(aStr) {
 
 this.addEventListener('install', function(event) {
   debug('install event');
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
-      ]);
-    })
-  );
 });
 
 // You could also do
@@ -47,17 +31,5 @@ this.addEventListener('install', function(event) {
 
 this.addEventListener('fetch', function(event) {
   debug('fetch event: ' + event.request.url);
-  var cachedResponse = caches.match(event.request).catch(function() {
-    return event.default().then(function(response) {
-      return caches.get('v1').then(function(cache) {
-        cache.put(event.request, response.clone());
-        return response;
-      });  
-    });
-  }).catch(function() {
-    return caches.match('/sw-test/gallery/myLittleVader.jpg');
-  });
-    
-  event.respondWith(cachedResponse);
 });
 
